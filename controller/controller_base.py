2b64b466514818612e21619b1abba006f7a65a98
artem690 <49416764+artem690@users.noreply.github.com>
2020-11-30 18:17:31 -0700

    Add files via upload

---
+++ controller/controller_base.py
@@ -0,0 +1,14 @@
+"""robot controller."""
+import math
+import copy
+from controller import Robot, Motor, DistanceSensor
+import supervisor
+import numpy as np
+from collections import defaultdict
+
+def main():
+    
+
+
+if __name__ == "__main__":
+    main()
