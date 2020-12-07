35f85de1dad6af67c66a1db828a6fb52212c850d
ryol2335 <74747602+ryol2335@users.noreply.github.com>
2020-12-07 12:26:22 -0700

    Update controller_base.py

--- controllers/controller_base/controller_base.py
+++ controllers/controller_base/controller_base.py
@@ -12,10 +12,93 @@ robot = supervisor.supervisor
 
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
-obstacle_duck1 = (0.07, 0.01, 0.95)
-obstacle_duck2 = (-0.150001, 0.01, 0.58)
-obstacle_duck3 = (0.79, 0.01, -0.03)
-obstacle_soda = (-0.18, 0.06, -0.17)
+
+# Robot Pose Values
+pose_x = 0
+pose_y = 0
+pose_theta = 0
+left_wheel_direction = 0
+right_wheel_direction = 0
+
+EPUCK_MAX_WHEEL_SPEED = 0.12880519 # m/s
+EPUCK_AXLE_DIAMETER = 0.053 # ePuck's wheels are 53mm apart.
+EPUCK_WHEEL_RADIUS = 0.0205 # ePuck's wheels are 0.041m in diameter.
+
+# Initialize Motors
+leftMotor = robot.getMotor('left wheel motor')
+rightMotor = robot.getMotor('right wheel motor')
+leftMotor.setPosition(float('inf'))
+rightMotor.setPosition(float('inf'))
+leftMotor.setVelocity(0.0)
+rightMotor.setVelocity(0.0)
+
+MAX_VEL_REDUCTION = 0.25
+
+def update_odometry(left_wheel_direction, right_wheel_direction, time_elapsed):
+  
+    global pose_x, pose_y, pose_theta, EPUCK_MAX_WHEEL_SPEED, EPUCK_AXLE_DIAMETER
+    pose_theta += (right_wheel_direction - left_wheel_direction) * time_elapsed * EPUCK_MAX_WHEEL_SPEED / EPUCK_AXLE_DIAMETER
+    pose_x += math.cos(pose_theta) * time_elapsed * EPUCK_MAX_WHEEL_SPEED * (left_wheel_direction + right_wheel_direction)/2.
+    pose_y += math.sin(pose_theta) * time_elapsed * EPUCK_MAX_WHEEL_SPEED * (left_wheel_direction + right_wheel_direction)/2.
+    pose_theta = get_bounded_theta(pose_theta)
+    
+def get_bounded_theta(theta):
+    
+    while theta > math.pi: theta -= 2.*math.pi
+    while theta < -math.pi: theta += 2.*math.pi
+    return theta
+
+def get_wheel_speeds(target_pose):
+    
+    global pose_x, pose_y, pose_theta, left_wheel_direction, right_wheel_direction
+
+    pose_x, pose_y, pose_theta = csci3302_lab5_supervisor.supervisor_get_robot_pose()
+
+
+    bearing_error = math.atan2( (target_pose[1] - pose_y), (target_pose[0] - pose_x) ) - pose_theta
+    distance_error = np.linalg.norm(target_pose[:2] - np.array([pose_x,pose_y]))
+    heading_error = target_pose[2] -  pose_theta
+
+    BEAR_THRESHOLD = 0.06
+    DIST_THRESHOLD = 0.03
+    dT_gain = theta_gain
+    dX_gain = distance_gain
+    if distance_error > DIST_THRESHOLD:
+        dTheta = bearing_error
+        if abs(bearing_error) > BEAR_THRESHOLD:
+            dX_gain = 0
+    else:
+        dTheta = heading_error
+        dX_gain = 0
+
+    dTheta *= dT_gain
+    dX = dX_gain * min(3.14159, distance_error)
+
+    phi_l = (dX - (dTheta*EPUCK_AXLE_DIAMETER/2.)) / EPUCK_WHEEL_RADIUS
+    phi_r = (dX + (dTheta*EPUCK_AXLE_DIAMETER/2.)) / EPUCK_WHEEL_RADIUS
+
+    left_speed_pct = 0
+    right_speed_pct = 0
+    
+    wheel_rotation_normalizer = max(abs(phi_l), abs(phi_r))
+    left_speed_pct = (phi_l) / wheel_rotation_normalizer
+    right_speed_pct = (phi_r) / wheel_rotation_normalizer
+    
+    if distance_error < 0.05 and abs(heading_error) < 0.05:    
+        left_speed_pct = 0
+        right_speed_pct = 0
+        
+    left_wheel_direction = left_speed_pct * MAX_VEL_REDUCTION
+    phi_l_pct = left_speed_pct * MAX_VEL_REDUCTION * leftMotor.getMaxVelocity()
+
+    right_wheel_direction = right_speed_pct * MAX_VEL_REDUCTION
+    phi_r_pct = right_speed_pct * MAX_VEL_REDUCTION * rightMotor.getMaxVelocity()
+
+
+    # print("Current pose: [%5f, %5f, %5f]\t\t Target pose: [%5f, %5f, %5f]\t\t %5f %5f %5f\t\t  %3f %3f" % (pose_x, pose_y, pose_theta, target_pose[0], target_pose[1], target_pose[2], bearing_error, distance_error, get_bounded_theta(heading_error), left_wheel_direction, right_wheel_direction))
+   
+    return phi_l_pct, phi_r_pct
+
 
 def hasObstacle():
     pass
